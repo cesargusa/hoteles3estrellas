5cabff1a2ccbd9012c137f8c79b87054d0d464c5
cesargusa22 <cesargusa22@gmail.com>
2023-09-16 15:04:12 +0200

    buscador boooking añadido y mejorado

---
+++ js/bookingSearch.js
@@ -0,0 +1,13 @@
+
+
+  (function () {
+      var BookingAffiliateWidget = new Booking.AffiliateWidget({
+"iframeSettings": {
+"selector": "bookingAffiliateWidget_22ea4926-0b32-4fa9-8aa3-abae4cf38006",
+"responsive": true
+},
+"widgetSettings": {}
+});
+  })();
+
+
